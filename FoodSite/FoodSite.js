cc60dbd2ce86a7a766fab868fdce4305378c0f49
unknown <wilfriedjuniikamga@gmail.com>
2022-11-12 03:21:03 +0100

    site en ligne

--- FoodSite/FoodSite.js
+++
@@ -1,7 +0,0 @@
-let menu=document.querySelector('#menu-bars');
-let navbar=document.querySelector('.navbar');
-
-menu.unclick=()=>{
-    menu.classList.toggle ('fa-times');
-    navbar.classList.toggle('active');
-}
